1aa2ac489f03f0eae025d79381019ef62c8bc39c
chaehee Kim <kche88@naver.com>
2023-11-27 20:24:08 +0900

    second commit

--- src/Pages/PhotosPage.js
+++ src/Pages/PhotosPage.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 
+
 const PhotosPage = () => {
   const { id } = useParams();
   const [article, setArticle] = useState({});
